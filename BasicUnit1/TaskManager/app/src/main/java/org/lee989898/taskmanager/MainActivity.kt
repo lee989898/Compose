ab2b8f5615937ce5e1c49d844433bbcb5a32b26e
lee989898 <wnsrud1352@gmail.com>
2023-04-05 15:27:19 +0900

    [ui] TaskCompletedScreen 디자인

--- BasicUnit1/TaskManager/app/src/main/java/org/lee989898/taskmanager/MainActivity.kt
+++ BasicUnit1/TaskManager/app/src/main/java/org/lee989898/taskmanager/MainActivity.kt
@@ -3,13 +3,19 @@ package org.lee989898.taskmanager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.lee989898.taskmanager.ui.theme.TaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,11 +24,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             TaskManagerTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background,
-                ) {
-                    Greeting("Android")
+                Surface {
+                    TaskCompletedScreen()
                 }
             }
         }
@@ -30,14 +33,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TaskCompletedScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        val image = painterResource(id = R.drawable.ic_task_completed)
+        Image(
+            painter = image,
+            contentDescription = null,
+        )
+        Text(
+            text = stringResource(id = R.string.all_task_completed),
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(top = 24.dp, bottom = 8.dp),
+        )
+        Text(
+            text = stringResource(id = R.string.nice_work),
+            fontSize = 16.sp,
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
+fun TaskCompletedPreview() {
     TaskManagerTheme {
-        Greeting("Android")
+        Surface {
+            TaskCompletedScreen()
+        }
     }
 }
